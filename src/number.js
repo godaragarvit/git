8162008d24cee5cdc3a12bb4556e74f54bfa08cb
Garvit Godara <garvit-godara@live.nmit.ac.nz>
2024-03-07 15:01:37 +1300

    number.js

---
+++ src/number.js
@@ -0,0 +1,31 @@
+//NOTE - This file is used to test the number of lines of code in the project
+/*
+
+*/
+// const pi = 3.14
+// //NOTE const does not allow you to change the value of the variable
+// console.log(pi)
+// let num2 = 20
+// // NOTE let allows you to change the value of the n
+// //num2 = 25
+// console.log(num2)
+// var num3 = 30
+// console.log(num3)
+// const FullName = "Garvit Godara"
+// console.log(FullName)
+// const study = "B.Tech in NMIT"
+// console.log(study)
+// console.log(FullName + " is studying " + study) //NOTE - This is how you concatenate strings in JS
+// console.log(`My full name is ${FullName} and I am studying ${study}`) //NOTE - This is how you concatenate strings in JS using template literals
+
+// let a = 2
+// let result
+// // if() {} else () {}
+// a = 0
+// if (a >= 0) {
+//     result = "Positive"
+//     console.log(result)
+// } else {
+//     result = "Negative"
+//     console.log(result)
+// }
